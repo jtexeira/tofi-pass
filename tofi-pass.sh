db21b3f4a689eb9661320982eadec63ec6c4adac
João Teixeira <jmt@eurotux.com>
2024-06-18 21:36:32 +0100

    Fix when user cancels

--- tofi-pass.sh
+++ tofi-pass.sh
@@ -8,14 +8,23 @@ then
     exit
 fi
 
+PASS=""
+RET=""
+
 # Get all the passwords
 PASS=$(find "$HOME/.password-store" -name '*.gpg' | sed "s|$HOME\/.password-store\/||g" | sed 's/.gpg//g')
 
 # Get the password to copy
 PASS=$(echo "$PASS" | tofi --prompt-text "Select a password to copy: " 2>/dev/null)
 
+# Exit if the user cancels
+if [ -z "$PASS" ]
+then
+    exit
+fi
+
 # Copy the password to the clipboard
-RET=$(pass show --clip "$PASS")
+RET=$(pass show --clip "$PASS_CHOICE")
 
 # Notify if notif-send is installed
 if command -v notify-send &> /dev/null
